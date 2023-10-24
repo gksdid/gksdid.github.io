e5e4782490af1c7445cecee05c7ff2eb50b49218
gksdid <148849448+gksdid@users.noreply.github.com>
2023-10-24 18:59:27 -0500

    Create common.js

---
+++ 2-medal/common.js
@@ -0,0 +1,5 @@
+// 페이지 맨 위로
+function scrollToTop() {
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+        }
