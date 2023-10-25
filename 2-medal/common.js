9adc5d3bd7dddf5de17efa562ed6b9cb987b1e9c
gksdid <148849448+gksdid@users.noreply.github.com>
2023-10-24 19:09:10 -0500

    Update common.js

--- 2-medal/common.js
+++ 2-medal/common.js
@@ -3,3 +3,12 @@ function scrollToTop() {
             document.body.scrollTop = 0; // For Safari
             document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
         }
+
+// header 로딩
+        document.addEventListener('DOMContentLoaded', function () {
+            fetch('path/to/header.html') // header.html의 경로를 설정
+                .then(response => response.text())
+                .then(data => {
+                    document.getElementById('page-header').innerHTML = data;
+                });
+        });
