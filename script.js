a12362ec02fadb79b4cb6d5f207471d083b5ae18
gksdid <148849448+gksdid@users.noreply.github.com>
2023-10-25 08:50:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function redirectToPage(page) {
     window.location.href = page;
 }
+
+
+// analytics.js (구글 분석)
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-4VK413KLDK');
