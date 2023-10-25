03ede48f4af54bcbc9e572c0076110d1f54e1af0
gksdid <148849448+gksdid@users.noreply.github.com>
2023-10-25 15:26:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,10 +10,3 @@ function gtag() {
 gtag('js', new Date());
 gtag('config', 'G-4VK413KLDK');
 
-// 페이지 헤더를 불러옵니다.
-import('./header.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('headerContainer').innerHTML = html;
-      })
-      .catch(error => console.error('Error loading header:', error));
