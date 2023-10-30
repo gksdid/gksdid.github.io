f639e56867db28720689ef2d72cc4c7c50069011
gksdid <148849448+gksdid@users.noreply.github.com>
2023-10-30 10:16:52 -0500

    Update js-datasetup.js

--- js-datasetup.js
+++ js-datasetup.js
@@ -47,17 +47,21 @@ function updateProbabilitySum() {
     });
 
     var resultElement = document.getElementById('probability-sum');
-    resultElement.textContent = '확률 합계: ' + sum.toFixed(2) + '%';
+    // resultElement.textContent = '확률 합계: ' + sum.toFixed(2) + '%';
+    resulttElement.innerHTML = '확률 합계: <br>' + sum.toFixed(2) + '%';
+
 
     var probabilityAttempts = calculateAttempts(sum);
     var attemptsElement = document.getElementById('probability-attempts');
-    attemptsElement.textContent = '평균 횟수: ' + probabilityAttempts + '회';
+    // attemptsElement.textContent = '평균 횟수: ' + probabilityAttempts + '회';
+    attemptsElement.innerHTML = '평균 횟수: <br>' + probabilityAttempts + ' 회';
 
     var costElement = document.getElementById('cost');
     var costValue = (probabilityAttempts / 10 * price).toFixed(2);
     var costInteger = parseInt(costValue, 10);
 
-    costElement.textContent = '평균 비용: ' + costInteger + ' UC';
+    // costElement.textContent = '평균 비용: ' + costInteger + ' UC';
+    costElement.innerHTML = '평균 비용: <br>' + costInteger + ' UC';
 
     var priceDisplay = document.getElementById('price-display');
     priceDisplay.textContent = price + ' UC';
