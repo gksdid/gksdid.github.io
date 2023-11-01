7402dab7eced5e692f6bbe69914db9b0e1c8e718
gksdid <148849448+gksdid@users.noreply.github.com>
2023-10-31 20:48:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function gtag() {
 gtag('js', new Date());
 gtag('config', 'G-4VK413KLDK');
 
+
+// 타이머
+document.addEventListener("DOMContentLoaded", function() {
+    // 각 HTML에서 날짜 설정
+    const targetDateAttribute = document.currentScript.getAttribute("data-target-date");
+    const targetDate = new Date(targetDateAttribute);
+
+    function updateCountdown() {
+        const currentDate = new Date();
+        const timeDifference = targetDate - currentDate;
+
+        const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        const countdownElement = document.getElementById("countdown");
+        countdownElement.innerHTML = `남은 시간: ${days}일 ${hours}시간 ${minutes}분 ${seconds}초`;
+    }
+
+    // 페이지 로드 시 초기 업데이트
+    updateCountdown();
+
+    // 1초마다 업데이트
+    setInterval(updateCountdown, 1000);
+});
+
+
+
